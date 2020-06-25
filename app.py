39db0d3d2641c74b733b2afd0cf2f09405e02758
nick <nick.silverman11@gmail.com>
2020-06-25 10:14:56 -0600

    major revision: live connection to Google Sheet, and new missouls specific features

--- app.py
+++ app.py
@@ -6,65 +6,32 @@ from datetime import datetime
 from datetime import timedelta
 from libs.obs_utils import *
 from libs.sir_utils import *
+from libs.gsheet import *
 
+# Google Sheets credentials
+SPREADSHEET_ID = "1ZHnIEjpFZ9U9Iu5VJfdTVKU2NiVBMtrvjDekRKsXmLs"
+SCOPE = ['https://www.googleapis.com/auth/spreadsheets',]
+GSHEETS_KEY = "MissoulaCovid19-1799de069711.json"
 
-# Bring in data and setup sidebar widgets =====================================
-# Select state sidebar
-state_names = np.sort(np.load('data/state_names.npy', allow_pickle=True))
-mt_idx = np.int(np.argwhere(state_names == 'Montana')[0][0])
-state_loc = st.sidebar.selectbox(
-    label='Choose State',
-    options = state_names,
-    index=mt_idx
-)
+# Bring in Google Sheets data
+gs_df = gsheet2df(SPREADSHEET_ID, GSHEETS_KEY, SCOPE)
+update = gs_df.Date.iloc[-1]
+mt_cases = gs_df['Active infected'].iloc[-1]
+zoo_cases = gs_df['Active Missoula'].iloc[-1]
 
-
-# Logic based on location
-if state_loc == 'Montana':
-    state_data = StateCovidData(state=state_loc).cov_update()
-    county_data = CountyCovidData(state=state_loc).cov_update()
-    default = 'Missoula'
-    mt_cases = int(state_data[state_data['location'] == 'Montana'].iloc[-1]['cases'])
-    zoo_cases = int(county_data[county_data['location'] == 'Missoula'].iloc[-1]['cases'])
-    np.save('data/current_cases', np.array([mt_cases, zoo_cases]))
-else:
-    state_data = StateCovidData(state=state_loc).cov_update(update=False)
-    county_data = CountyCovidData(state=state_loc).cov_update(update=False)
-    default = county_data['location'].iloc[0]
-    current_cases = np.load('data/current_cases.npy', allow_pickle=True)
-    mt_cases = int(current_cases[0])
-    zoo_cases = int(current_cases[1])
-
-# Select county sidebar
-location = st.sidebar.multiselect(
-    label='Choose County:',
-    options=list(np.sort(county_data['location'].unique())),
-    default=default
-)
-
-if not location:
-    st.error("Please select at least one county")
-
-st.sidebar.markdown(
-    """
-    [View MT Model Comparison](http://mtmodcomp.missoulacov19.com)
-    """
-)
-
-# Title and update ===============================================================
+# Title
 st.title('Missoula Covid-19 Dashboard')
-update = state_data.index[-1].strftime("%m/%d/%Y")
 st.text('Last update: {}'.format(update))
 st.text("")
 st.text("")
 
-# Current status in Montana and Missoula (static)
+# Current active status in Montana and Missoula (static)
 st.markdown(
     """
-    |        | Total Cases |
-    |--------|-------------|
-    |Montana |{mt_cases}   |
-    |Missoula|{zoo_cases}  |
+    |        | Total Active Cases |
+    |--------|--------------------|
+    |Montana |{mt_cases}          |
+    |Missoula|{zoo_cases}         |
     """.format(
         mt_cases=mt_cases,
         zoo_cases=zoo_cases,
@@ -72,6 +39,127 @@ st.markdown(
 )
 st.text("")
 st.text("")
+
+st.markdown(
+    """
+    ### Active Cases
+    The number of active cases of Covid-19 in Montana (orange) and Missoula (blue). 
+    To get exact numbers select the checkbox below or zoom and hover on the chart.
+    """
+)
+
+# Format Gsheet data
+gs_df.Date = pd.to_datetime(gs_df.Date)
+gs_df.set_index('Date', inplace=True)
+gs_df.replace("", np.nan, inplace=True)
+active_df = gs_df[['Active infected', 'Active Missoula']]
+active_df.columns = ['Montana', 'Missoula']
+active_df = active_df.dropna(axis=0, how='all')
+testing_df = pd.DataFrame(gs_df['Tests completed'])
+testing_df.columns = ['Tests Completed']
+
+# Plot active cases
+active_df.index = active_df.index.strftime("%Y-%m-%d")
+if st.checkbox('Show Raw Data For Active Cases'):
+    st.write(
+        '#### Active cases:', 
+        active_df.sort_index(ascending=False)
+        )
+
+# Plot active cases
+active_df['Date'] = pd.to_datetime(active_df.index)
+active_df['Montana'] = pd.to_numeric(active_df.loc[:, 'Montana'])
+active_df['Missoula'] = pd.to_numeric(active_df.loc[:, 'Missoula'])
+active_melt = pd.melt(
+    active_df,
+    id_vars='Date',
+    var_name = 'Location',
+    value_name='Active Cases'
+)
+
+active_chart = (
+    alt.Chart(active_melt)
+    .mark_line()
+    .encode(
+        x='Date',
+        y='Active Cases',
+        color='Location',
+        tooltip=['Date', 'Active Cases']
+    )
+).interactive()
+
+st.altair_chart(active_chart, use_container_width=True)
+
+# Plot testing data
+st.markdown(
+    """
+    ### Daily Covid-19 Tests Completed in Montana
+    The number of daily Covid-19 tests administered in Montana.
+    To get exact numbers select the checkbox below or zoom and hover on the chart.
+    """
+)
+
+testing_df.index = testing_df.index.strftime("%Y-%m-%d")
+if st.checkbox('Show Raw Data For Daily Tests Completed'):
+    st.write(
+        '#### Tests Completed:', 
+        testing_df.sort_index(ascending=False)
+        )
+
+
+testing_df['Date'] = pd.to_datetime(testing_df.index)
+testing_df['Tests Completed'] = pd.to_numeric(testing_df.loc[:,'Tests Completed'])
+
+testing_chart = (
+    alt.Chart(testing_df)
+    .mark_area(
+        line=True, 
+        opacity=0.4, 
+        interpolate='step-after'
+    )
+    .encode(
+        x='Date',
+        y='Tests Completed',
+        tooltip = ['Date', 'Tests Completed']
+    )
+).interactive()
+st.altair_chart(testing_chart, use_container_width=True)
+
+# Select state 
+st.markdown(
+    """
+    ### Explore Data From Other Counties
+    In the section below you can select other States and Counties to explore their total 
+    confirmed cases, daily confirmed cases, and doubling time (use the checkbox in the second chart). 
+    To get exact numbers select the checkbox for total confirmed cases or zoom and hover on the chart.
+    """
+)
+
+state_names = np.sort(np.load('data/state_names.npy', allow_pickle=True))
+mt_idx = np.int(np.argwhere(state_names == 'Montana')[0][0])
+state_loc = st.selectbox(
+    label='Choose State',
+    options = state_names,
+    index=mt_idx
+)
+
+state_data = StateCovidData(state=state_loc).cov_update(update=False)
+county_data = CountyCovidData(state=state_loc).cov_update(update=False)
+
+if state_loc == 'Montana':    
+    default = 'Missoula'
+else:
+    default = county_data['location'].iloc[0]
+
+# Select county 
+location = st.multiselect(
+    label='Choose County:',
+    options=list(np.sort(county_data['location'].unique())),
+    default=default
+)
+
+if not location:
+    st.error("Please select at least one county")
 
 # Create one dataframe
 county_loc = county_data.loc[county_data['location'].isin(location)][['location','cases']]
@@ -101,7 +189,7 @@ df_loc_melt = pd.melt(
 # Create checkbox to view dataframe
 df_show = df_loc.copy()
 df_show.index = df_loc.index.strftime("%Y-%m-%d")
-if st.checkbox('Show Covid-19 raw data'):
+if st.checkbox('Show Raw Data For Total Confirmed Cases'):
     st.write(
         '#### Number of Confirmed Covid-19 Cases:', 
         df_show.sort_index(ascending=False)
@@ -165,15 +253,7 @@ st.altair_chart(chart_diff, use_container_width=True)
 st.markdown(
     """
     ### What's new?
-
-    - Added model comparison for Montana (see link in sidebar)
-
-    - All states and counties have been added.
-
-    - Added approximate doubling time based on the percent change from the 
-    previous day.
-
-    - Can now hover over an area and get the value on both charts.   
+    New layout with Missoula specific charts added (6/25/2020)
 
     ### Data sources
     [New York Times](<https://github.com/nytimes/covid-19-data>)
